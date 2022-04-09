90fd12ae73e6fd877c480c95e295fc27172961b1
Lewis Moten <lewis.moten@codejamboree.com>
2022-04-09 18:29:53 -0400

    :goal_net: prepare to fail

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,40 @@
+import React, { Component, ErrorInfo, ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+  onError?: (error: Error, errorInfo: ErrorInfo) => void;
+  fallback?: ReactNode;
+}
+
+interface State {
+  failed: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  public state: State = { failed: false };
+  static getDerivedStateFromError(_: Error): State {
+    return { failed: true };
+  }
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    const { onError } = this.props;
+    if (typeof onError !== "function") return;
+    try {
+      onError(error, errorInfo);
+    } catch (e) {
+      // do nothing
+    }
+  }
+  public render() : JSX.Element {
+    const { fallback = "Error", children } = this.props;
+    return <Wrap>{this.state.failed ? fallback : children}</Wrap>;
+  }
+}
+
+interface ElementProps {
+  children: ReactNode;
+}
+
+const Wrap = ({ children }: ElementProps): JSX.Element =>
+  React.isValidElement(children) ? children : <>{children}</>;
+
+export default ErrorBoundary;
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ErrorBoundary from './components/ErrorBoundary';
 
-ReactDOM.render((<React.StrictMode><div>Hello World</div></React.StrictMode>), document.getElementById("root"));
+ReactDOM.render(
+    (
+    <React.StrictMode>
+        <ErrorBoundary>
+            <div>Hello World</div>
+        </ErrorBoundary>
+    </React.StrictMode>
+    ), document.getElementById("root"));
