faeab94ddcea2d7ef85ebdf26b0081b179868f52
Lewis Moten <lewis.moten@codejamboree.com>
2022-04-10 14:19:25 -0400

    :lipstick: swap light/dark mode based on os settings

--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -1,15 +1,37 @@
+import { useMemo } from 'react';
+
 import Header from './Header';
 import Footer from './Footer';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 interface ITemplate {
     children: JSX.Element;
 }
 
-const Template = ({ children }: ITemplate) => 
-<>
-    <Header />
-    {children}
-    <Footer />
-</>
+const Template = ({ children }: ITemplate) => {
+
+    const prefersDarkColorScheme = useMediaQuery('(prefers-color-scheme: dark)');
+
+    const theme = useMemo(
+        () =>
+          createTheme({
+            palette: {
+              mode: prefersDarkColorScheme ? 'dark' : 'light',
+            },
+          }),
+        [prefersDarkColorScheme]
+      );
+    
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Header />
+            {children}
+            <Footer />
+        </ThemeProvider>
+    );
+}
 
 export default Template;
