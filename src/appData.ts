2edbc0bc18ef250501fa5d24dc44d3b5d69fb333
Lewis Moten <lewis.moten@codejamboree.com>
2022-04-09 20:14:28 -0400

    :wrench: add name/copyright

---
+++ src/appData.ts
@@ -0,0 +1,6 @@
+const data = {
+    name: "Code Jamboree™",
+    license: "\xa9 Copyright 2022, Code Jamboree LLC"
+}
+
+export default data;
--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -1,9 +1,18 @@
 import { ReactNode } from "react";
+import appData from '../appData';
 
 interface ITemplate {
     children: JSX.Element;
 }
 
-const Template = ({ children }: ITemplate) => children;
+const Template = ({ children }: ITemplate) => 
+<div>
+    {appData.name}
+    <hr />
+    {children}
+    <hr />
+    {appData.license}
+
+</div>
 
 export default Template;
