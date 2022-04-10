9ea2c855c0f587756a4935fbb240ac6046679fd5
Lewis Moten <lewis.moten@codejamboree.com>
2022-04-10 16:02:46 -0400

    :lipstick: Setup theme similar to our brands colors

--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -5,6 +5,8 @@ import Footer from './Footer';
 import CssBaseline from '@mui/material/CssBaseline';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
+import Button from '@mui/material/Button';
+import buildTheme from './buildTheme';
 
 interface ITemplate {
     children: JSX.Element;
@@ -13,14 +15,9 @@ interface ITemplate {
 const Template = ({ children }: ITemplate) => {
 
     const prefersDarkColorScheme = useMediaQuery('(prefers-color-scheme: dark)');
-
     const theme = useMemo(
         () =>
-          createTheme({
-            palette: {
-              mode: prefersDarkColorScheme ? 'dark' : 'light',
-            },
-          }),
+        buildTheme(prefersDarkColorScheme),
         [prefersDarkColorScheme]
       );
     
@@ -29,6 +26,36 @@ const Template = ({ children }: ITemplate) => {
             <CssBaseline />
             <Header />
             {children}
+<p>
+            <Button color="primary">primary</Button>
+            <Button color="primary" variant="contained">contained</Button>
+            <Button color="primary" variant="outlined">outlined</Button>
+ </p>
+ <p>
+            <Button color="secondary">secondary</Button>
+            <Button color="secondary" variant="contained">contained</Button>
+            <Button color="secondary" variant="outlined">outlined</Button>
+ </p>
+ <p>
+            <Button color="info">info</Button>
+            <Button color="info" variant="contained">contained</Button>
+            <Button color="info" variant="outlined">outlined</Button>
+ </p>
+ <p>
+            <Button color="success">success</Button>
+            <Button color="success" variant="contained">contained</Button>
+            <Button color="success" variant="outlined">outlined</Button>
+ </p>
+ <p>
+            <Button color="warning">warning</Button>
+            <Button color="warning" variant="contained">contained</Button>
+            <Button color="warning" variant="outlined">outlined</Button>
+ </p>
+ <p>
+            <Button color="error">error</Button>
+            <Button color="error" variant="contained">contained</Button>
+            <Button color="error" variant="outlined">outlined</Button>
+ </p>
             <Footer />
         </ThemeProvider>
     );
---
+++ src/components/buildTheme.ts
@@ -0,0 +1,66 @@
+import { createTheme } from '@mui/material/styles';
+
+/*
+Colors are from our company logo
+
+Business Cards have a dark blue background
+
+{...} (C & J as curly braces, with ellipses/code in between)
+
+and Company Name
+
+*/
+const primaryFontName = "Avenir";
+const darkIndegoBusinessCardBackground = '#000961';
+const lightBlueC = '#0094da';
+const lightGreenEllipses = '#93caa7';
+const purpleJ = '#662483';
+const purpleShadowJ = '#502264';
+const grayTradeName = '#555454';
+const yellowForShadow = '#d4d644'; // not in our logo
+
+const palette = {
+    primary: {
+        main: darkIndegoBusinessCardBackground,
+    },
+    secondary: {
+        main: grayTradeName,
+    },
+    info: {
+        main: lightBlueC,
+    },
+    success: {
+        main: lightGreenEllipses,
+    },
+    warning: {
+        main: purpleJ,
+    },
+    error: {
+        main: purpleShadowJ,
+        contrastText: yellowForShadow
+    },
+    contrastThreshold: 3,
+    tonalOffset: 0.2
+}
+
+// Avenir is the font used within our branding
+const fontFamily = [
+    primaryFontName,
+    "Montserrat", // Similar to Avenir
+    "Roboto",
+    "Helvetica",
+    "Arial",
+    "sans-serif"
+].map(name => `"${name}"`).join(",");
+
+const buildTheme = (prefersDarkColorScheme: boolean) => createTheme({
+    typography: {
+      fontFamily
+    },
+    palette: {
+      mode: prefersDarkColorScheme ? 'dark' : 'light',
+        ...palette
+      }
+})
+
+export default buildTheme;
