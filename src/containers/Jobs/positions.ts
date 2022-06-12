0a38f1df3af246aeb69569bec5df6c67f4029ed2
Lewis Moten <lewis.moten@codejamboree.com>
2022-06-12 13:23:27 -0400

    :lipstick: add dev icons

--- src/containers/Jobs/positions.ts
+++ src/containers/Jobs/positions.ts
@@ -1,6 +1,10 @@
+// icons can be found at https://devicon.dev/
+
+
 interface IDetail {
     header?: boolean,
-    text: string
+    text: string,
+    icons?: string[]
 }
 interface IPosition {
     title: string,
@@ -19,23 +23,55 @@ let position = {
     details
 } as IPosition;
 
-details.push({text: `
+details.push({
+    text: `
     Use React functional components to query external sources
     while displaying a loading state. Work with both context
     and local state. Design responsive pages for desktop and
     mobile platforms. Write unit tests to confirm features
-    work and are not broken in future developement.
+    work and are not broken in future developement. Document
+    components using markdown. Build packages with webpack
+    using nodejs and deploy to npm. Work in an agile/scrum
+    environment.
 `});
-details.push({text: 'Skills needed', header: true})
-details.push({text: `
-    React, React Hooks, git, node, JavaScript, eslint,
-    issue tracking, unit testing
-`})
-details.push({text: 'Skills preferred', header: true})
-details.push({text: `
-Typescript, Angular, Redux, Redux-Saga, sass/scss, webpack, JIRA,
-GitHub, HTML Canvas, Agile Development, Jest, Jasmine, Jenkins
-`})
+details.push({
+    text: 'Skills needed', header: true})
+details.push({
+    text: `
+    JavaScript, React, React Hooks, Git, Node, NPM, Eslint,
+    issue tracking, unit testing, Markdown, HTML, CSS
+`, icons: [
+    'javascript',
+    'react',
+    'git',
+    'nodejs',
+    'npm',
+    'eslint',
+    'markdown',
+    'html5',
+    'css3'
+]
+})
+details.push({
+    text: 'Skills desired', header: true
+})
+details.push({
+    text: `
+    Typescript, Angular, Redux, Redux-Saga, sass, Webpack,
+    JIRA, GitHub, HTML Canvas, Agile Development, Jest, Jasmine,
+    Jenkins
+`, icons: [
+    'typescript',
+    'angularjs',
+    'redux',
+    'sass',
+    'webpack',
+    'jira',
+    'github',
+    'jest',
+    'jasmine',
+    'jenkins'
+]})
 positions.push(position);
 // -----------------------------
 position = {
@@ -45,9 +81,14 @@ position = {
     details: []
 } as IPosition;
 
-position.details.push({text: `
-  Proficient in Java, React, Python, Angular, Kafka...
-`});
+position.details.push({
+    text: `
+    We have a few various projects where we need developers
+    proficient in Java, React, Python, Angular, Kafka, Go, AWS...
+`, icons: [
+    'java', 'react', 'python', 'angularjs', 'apachekafka',
+    'go', 'amazonwebservices'
+]});
 positions.push(position);
 
 export default positions;
