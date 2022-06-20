50f5fed057ca8bf7c6332468eaafdfcf01f87a3c
Lewis Moten <lewis.moten@codejamboree.com>
2022-06-19 22:04:54 -0400

    :bug: get manifest working in dev mode

--- configuration/webpack.common.js
+++ configuration/webpack.common.js
@@ -2,6 +2,11 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+
+const TITLE = "Code Jamboree LLC";
+const SHORT_TITLE = "Code Jamboree";
+const DESCRIPTION = "Consulting for Software as a Service (SaaS)";
 
 module.exports = () => {
   const config = {
@@ -15,16 +20,43 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         publicPath: "/",
-        title: "Code Jamboree LLC",
+        title: TITLE,
         meta: {
           "Content-Type": {
             "http-equiv": "Content-Type",
             content: "text/html; charset=utf-8",
           },
-          description:
-            "IT Consulting Services for frontend, backend, and fullstack web development.",
+          description: DESCRIPTION,
           viewport: "initial-scale=1, width=device-width",
         },
+      }),
+      new WebpackPwaManifest({
+        name: TITLE,
+        short_name: SHORT_TITLE,
+        description: DESCRIPTION,
+        start_url: "/",
+        crossorigin: "use-credentials",
+        lang: "en-US",
+        inject: true,
+        ios: true,
+        filename: "manifest.webmanifest",
+        publicPath: "/",
+        fingerprints: false,
+        icons: [
+          {
+            src: path.resolve(__dirname, "../src/assets/logo512x512.png"),
+            destination: "icons",
+            sizes: [384],
+            purpose: "maskable",
+            ios: true,
+          },
+          {
+            src: path.resolve(__dirname, "../src/assets/logo512x512.png"),
+            destination: "icons",
+            sizes: [192, 512],
+            purpose: "maskable",
+          },
+        ],
       }),
       new webpack.EnvironmentPlugin(["NODE_ENV"]),
       new webpack.ProvidePlugin({
